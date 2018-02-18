4cee90269bac6504198d11071fbc646801990742
Adam Hodges <adam@ajhodges.com>
2018-02-18 11:40:27 -0500

    Utilize lambda event args. Use s3 client upload_file()

--- lambda_function.py
+++ lambda_function.py
@@ -10,12 +10,21 @@ print('Loading function')
 
 def lambda_handler(event, context):
     print("Received event: " + json.dumps(event))
-    args = ['perl', '-I', 'lib', 'zap2xml.pl' ]
+    client = boto3.client('s3')
 	
+    ZAP_OR_TVG = os.environ['ZAP_OR_TVG']
     FILE_NAME = os.environ['FILE_NAME']
     BUCKET_NAME = os.environ['BUCKET_NAME']
+    
+    if 'ZAP_OR_TVG' in event:
+        ZAP_OR_TVG = event['ZAP_OR_TVG']
+        
+    if 'FILE_NAME' in event:
+        FILE_NAME = event['FILE_NAME']
+    
+    args = ['perl', '-I', 'lib', 'zap2xml.pl' ]
 
-    if os.environ['ZAP_OR_TVG'] == 'TVG':
+    if ZAP_OR_TVG == 'TVG':
         args.append('-z')
         args.extend(('-u', os.environ['TVG_USERNAME']))
         args.extend(('-p', os.environ['TVG_PASSWORD']))
@@ -34,7 +43,4 @@ def lambda_handler(event, context):
     stdout_value, stderr_value = proc.communicate()
     print('Output:'  + stdout_value + stderr_value);
 	
-    data = open('/tmp/%s' % FILE_NAME, 'r')
-    s3 = boto3.resource('s3')
-    
-    s3.Bucket(os.environ['BUCKET_NAME']).put_object(Key=FILE_NAME, Body=data, ACL='public-read')
+    client.upload_file('/tmp/%s' % FILE_NAME, BUCKET_NAME, FILE_NAME)
